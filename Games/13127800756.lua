64c25d42e3c5d8babef268eeedc43fae4fbb1a9c
ManyPeopleKnowMe <140022627+ManyPeopleKnowMe@users.noreply.github.com>
2023-07-19 18:08:42 -0400

    Create 13127800756.lua

---
+++ Games/13127800756.lua
@@ -0,0 +1,8 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "💦 Jizz Hub - Arm Wrestle Simulator", HidePremium = false, SaveConfig = false, IntroEnabled = false})
+
+local HomeTab = Window:MakeTab({
+	Name = "Home",
+	Icon = "rbxassetid://12890941259",
+	PremiumOnly = false
+})
